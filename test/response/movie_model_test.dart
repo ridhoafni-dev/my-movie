2a0e6054c0cbef6599e17382fa1871ead29c2b85
My Name <ridhoafni.dev@gmail.com>
2024-11-04 09:29:26 +0700

    revised submission

---
+++ test/response/movie_model_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:model/movie/movie.dart';
+import 'package:response/movie/movie_model.dart';
+
+void main() {
+  const tMovieModel = MovieModel(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: '2020-05-05',
+    title: 'title',
+    voteAverage: 1,
+    voteCount: 1,
+    video: false,
+  );
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: const [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: '2020-05-05',
+    title: 'title',
+    voteAverage: 1,
+    voteCount: 1,
+    video: false,
+  );
+
+  test("should be a subclass of Movie entity", () async {
+    final result = tMovieModel.toEntity();
+
+    expect(result, tMovie);
+  });
+}
---
+++ test/response/movie_response_model_test.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:response/movie/movie_model.dart';
+import 'package:response/movie/movie_response.dart';
+import 'package:utils/utils/json_reader.dart';
+
+void main() {
+  const tMovieModel = MovieModel(
+    adult: false,
+    backdropPath: "/path.jpg",
+    genreIds: [1, 2, 3, 4],
+    id: 1,
+    originalTitle: "Original Title",
+    overview: "Overview",
+    popularity: 1.0,
+    posterPath: "/path.jpg",
+    releaseDate: "2020-05-05",
+    title: "Title",
+    voteAverage: 1.0,
+    voteCount: 1,
+    video: false,
+  );
+
+  const tMovieResponseModel =
+      MovieResponse(movieList: <MovieModel>[tMovieModel]);
+
+  group("from json", () {
+    test("should return valid model from json", () async {
+      // arrange
+      final Map<String, dynamic> jsonMap =
+      json.decode(readJson("dummy_data/movie_recommendations.json"));
+
+      // act
+      final result = MovieResponse.fromJson(jsonMap);
+
+      // assert
+      expect(result, tMovieResponseModel);
+    });
+
+  });
+
+  group('toJson', () {
+    test('should return a JSON map containing proper data', () async {
+      // arrange
+
+      // act
+      final result = tMovieResponseModel.toJson();
+      // assert
+      final expectedJsonMap = {
+        "results": [
+          {
+            'adult': false,
+            'backdrop_path': '/path.jpg',
+            'genre_ids': [1, 2, 3, 4],
+            'id': 1,
+            'original_title': 'Original Title',
+            'overview': 'Overview',
+            'popularity': 1.0,
+            'poster_path': '/path.jpg',
+            'release_date': '2020-05-05',
+            'video': false,
+            'title': 'Title',
+            'vote_average': 1.0,
+            'vote_count': 1
+          }
+        ],
+      };
+      expect(result, expectedJsonMap);
+    });
+  });
+}
